6c1f1a2bf19dfe867f5cf6d316e3b4e4562f2045
Jahasiel Serna <jh.serna.b@alumnos.udg.com>
2020-09-29 12:09:32 -0500

    número e

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func factorial(n float64) float64 {
+	if n <= 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	var n, e, i float64
+	fmt.Scan(&n)
+	for i = 0; i <= n; i++ {
+		e += 1 / factorial(i)
+	}
+	fmt.Println(e)
 }
